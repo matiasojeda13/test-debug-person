fdd23a4dd036029fa0d6589114becb98e506ff0c
matiasojeda13 <matias.ojeda@correo.ucu.edu.com>
2020-09-12 20:02:55 -0300

    test creados

--- test/Library.Test/PersonTests.cs
+++ test/Library.Test/PersonTests.cs
@@ -1,21 +1,32 @@
 using NUnit.Framework;
-
 using UnitTestAndDebug;
 
 namespace Tests
 {
     public class Tests
     {
+        private Person person;
+
         [SetUp]
         public void Setup()
         {
             // Insertá tu código de inicialización aquí
+            var person = new Person("Matias Ojeda","4.548.798-8");
         }
 
         [Test]
-        public void Test1() // Cambiá el nombre para indicar qué estás probando
+        public void NombreNoVacio() // Cambiá el nombre para indicar qué estás probando
         {
-            // Insertá tu código  de pruebaaquí
+            // Insertá tu código  de prueba aquí
+            string resultado = "Matias Ojeda";
+            Assert.AreEqual(person.Name,resultado);
+        }
+        [Test]
+        public void DigitoVerificadorCorrecto() // Cambiá el nombre para indicar qué estás probando
+        {
+            // Insertá tu código  de prueba aquí
+            string resultado = "4.548.798-8";
+            Assert.AreEqual(person.ID,resultado);
         }
     }
 }
